5a0517d5f04a34ec3da623c1dc025152205410b3
Kai-Leon van Son <KaiLeon.vanSon@schueler.ers-hm.de>
2021-12-16 19:46:16 +0100

    feature: Added Command Handler feature: Added Command prefix. Created Command Folder BugFix: transferred Ping Command into a Command File

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
 require('dotenv').config()
-const { Client, Intents} = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const fs = require('fs')
+const { Client, Intents, Collection} = require('discord.js')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+const prefix = "$"
+client.commands = new Collection();
 
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  console.log(file);
+  const command = require(`../commands/${file}`);
+  client.commands.set(command.name, command);
+}
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', (Message) => {
-    if(Message.author.bot) return
-
-    if (Message.content === 'ping') {
-        Message.channel.send('pong')
-    }
+  console.log(`Logged in as ${client.user.tag}!`)
 })
 
+client.on('message', (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (!client.commands.has(command)) return;
+
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('there was an error trying to execute that command!');
+  }
+})
 
 client.login(process.env.VANBETTING_BOT_TOKEN)
